2e681260acd6d4f254ee54941e735cee544956e1
Sean Stuckless <sean.stuckless@gmail.com>
2016-09-11 09:23:13 -0400

    Updated plugin manifest and build files to push to bintray

--- src/org/jdna/bmt/web/client/Version.java
+++ src/org/jdna/bmt/web/client/Version.java
@@ -2,6 +2,6 @@
 			package org.jdna.bmt.web.client;
 
 			public final class Version {
-			    public static final String VERSION = "4.99";
+			    public static final String VERSION = "4.100.0";
 			}
 		
